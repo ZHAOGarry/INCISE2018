e85d3d0bb60a493ae07b38c15a8be32a64d0e1b5
Zirui ZHAO <zhao-garry@outlook.com>
2018-04-21 16:34:17 +0800

    添加项目文件。

---
+++ INCISE2018-MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(INCISE2018_MVC.Startup))]
+namespace INCISE2018_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
